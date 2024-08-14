f3c64ec2f6d1de0e375db615e92cf45b8cda86d7
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-14 12:39:05 +0300

    implement user and role repos

---
+++ backend/src/main/java/com/books/book_store/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.books.book_store.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String role);
+}
