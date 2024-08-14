0908979dcd87feeae2b8aec5a2f20d9926f5c4b3
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-14 15:02:14 +0300

    implement jwt

---
+++ backend/src/main/java/com/books/book_store/security/UserDetailsService.java
@@ -0,0 +1,21 @@
+package com.books.book_store.security;
+
+import com.books.book_store.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+}
