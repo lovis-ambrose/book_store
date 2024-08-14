9a2de0114b63a61959502775f62d912c72b782bf
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-14 21:36:12 +0300

    fix role

--- backend/src/main/java/com/books/book_store/BookStoreApplication.java
+++ backend/src/main/java/com/books/book_store/BookStoreApplication.java
@@ -1,7 +1,11 @@
 package com.books.book_store;
 
+import com.books.book_store.role.Role;
+import com.books.book_store.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,4 +18,12 @@ public class BookStoreApplication {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner (RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("ROLE_USER").build());
+			}
+		};
+	}
 }
