b50c1ed39bd7424b9af9ac7b3aba4170210b0921
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-19 09:50:11 +0300

    fix multiple emails

--- backend/src/main/java/com/books/book_store/email/EmailService.java
+++ backend/src/main/java/com/books/book_store/email/EmailService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
