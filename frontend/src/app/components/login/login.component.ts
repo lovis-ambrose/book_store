9dbd9b51fbd796b0873bc0ae679e479e3efb01d6
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-23 23:06:53 +0300

    small fix

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,34 +1,35 @@
-import { Component } from '@angular/core';
-import {AuthenticationRequest} from "../../services/models/authentication-request";
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../services/services';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/services/authentication.service';
+import {AuthenticationRequest} from '../../services/models/authentication-request';
+// import {TokenService} from '../../services/token/token.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  errorMsg: Array<String> = [];
-  authRequest: AuthenticationRequest = {
-    email: "",
-    password: "",
-  };
+
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
 
   constructor(
     private router: Router,
-    private authService: AuthenticationService
-  ) {}
+    private authService: AuthenticationService,
+    // private tokenService: TokenService
+  ) {
+  }
 
   login() {
     this.errorMsg = [];
     this.authService.authenticate({
       body: this.authRequest
     }).subscribe({
-      next: () => {
-        // todo save the token
+      next: (res) => {
+        // this.tokenService.token = res.token as string;
         this.router.navigate(['books']);
-    },
+      },
       error: (err) => {
         console.log(err);
         if (err.error.validationErrors) {
@@ -41,6 +42,6 @@ export class LoginComponent {
   }
 
   register() {
-    this.router.navigate(["register"]);
+    this.router.navigate(['register']);
   }
 }
